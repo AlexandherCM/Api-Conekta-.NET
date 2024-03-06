0a2f4165a525dd31d9b851d86ba71a38d26c8908
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2024-03-06 02:37:54 -0600

    AddImage

--- Api_OxxoPay/Api_OxxoPay/Views/OxxoPay.aspx.cs
+++ Api_OxxoPay/Api_OxxoPay/Views/OxxoPay.aspx.cs
@@ -1,61 +1,79 @@
-﻿using System;
-using Conekta.net.Client;
-using Conekta.net.Api;
-using Conekta.net.Model;
-using static Conekta.net.Model.PlanRequest;
+﻿using Api_OxxoPay.Classes;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Api_OxxoPay.Views
 {
     public partial class OxxoPay : System.Web.UI.Page
     {
-        private Configuration _config = new Configuration()
-        {
-            AccessToken = "key_pq42sBrvzq9u0WnQ6aZvtrg"
-        };
-
-        private string _acceptLanguage = "es";
 
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
 
-        protected void BtnSend_Click(object sender, EventArgs e)
+        protected async void BtnSend_Click(object sender, EventArgs e)
         {
-            var customersApi = new CustomersApi(_config);
-            var ordersApi = new OrdersApi(_config);
-
-            var customer = new Customer(
-                name: "Alexandher Cordoba",
-                email: "alexandhercordoba378@gmail.com",
-                phone: "+525591889796"
-            );
-
-            CustomerResponse customerResponse = customersApi.CreateCustomer(customer);
-
-            var plansApi = new PlansApi(_config);
-
-            var interval = IntervalEnum.Month;
-            var planRequest = new PlanRequest
+            Root root = new Root()
             {
-                Name = "Hola mundo",
-                Amount = 100 *100, // Monto en centavos
-                Currency = "MXN", // Moneda
-                Interval = interval, // Intervalo de facturación
-                Frequency = 1, // Frecuencia de facturación
-                TrialPeriodDays = 2,
-                ExpiryCount = 1
+                amount = "200",
+                currency = "MXN",
+                name = "Alexandher Cordoba",
+                email = "alexandhercordoba378@gmail.com",
+                phone = "5591889796",
+                merchantReferenceCode = "dfscxvsdf",
+                oxxoPay = new Pay()
+                {
+                    expirationNumber = 1,
+                    expirationType = "DAY"
+                }
             };
 
-            PlanResponse planResponse = plansApi.CreatePlan(planRequest, _acceptLanguage);
+            string json = JsonConvert.SerializeObject(root);
 
-            var subscriptionRequest = new SubscriptionRequest(
-                planId: planResponse.Id
-            );
 
-            var subscriptionsApi = new SubscriptionsApi(_config);
-            SubscriptionResponse subscriptionResponse = subscriptionsApi.CreateSubscription(customerResponse.Id, subscriptionRequest, _acceptLanguage);
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri("https://gateway-154.netpaydev.com/gateway-ecommerce/v3/oxxopay/reference"),
+                Headers =
+                {
+                    { "accept", "application/json" },
+                },
+                Content = new StringContent("{\"amount\":\"20\",\"currency\":\"MXN\",\"name\":\"Jon\",\"email\":\"review@netpay.com.mx\",\"phone\":\"8100000222\",\"merchantReferenceCode\":\"referencia-unica-1133\",\"oxxoPay\":{\"expirationNumber\":10,\"expirationType\":\"DAY\"}}")
+                {
+                    Headers =
+        {
+            ContentType = new MediaTypeHeaderValue("application/json")
+        }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                //response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Response.Write(body);
+            }
+        }
+
+        public class Pay
+        {
+            public int expirationNumber { get; set; }
+            public string expirationType { get; set; }
+        }
+
+        public class Root
+        {
+            public string amount { get; set; }
+            public string currency { get; set; }
+            public string name { get; set; }
+            public string email { get; set; }
+            public string phone { get; set; }
+            public string merchantReferenceCode { get; set; }
+            public Pay oxxoPay { get; set; }
         }
     }
-
 }
